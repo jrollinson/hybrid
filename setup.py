8bd91b77b6d714d63b982143a48a1952137f9e5d
Joseph Rollinson <JTRollinson@gmail.com>
2014-11-26 12:40:34 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='hybrid',
+      version='0.1',
+      description='An Implementation of Hybrid Student Modelling',
+      author='Joseph Rollinson',
+      author_email='jtrollinson@gmail.com',
+      py_modules=['hybrid'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'scikit-learn'])
+
